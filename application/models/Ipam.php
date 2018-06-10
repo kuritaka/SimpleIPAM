8407888f2b6fcaa8e339e5a09f0946b6880300ab
sogaoh <sogaoh@outlook.com>
2018-06-10 21:08:58 +0900

    adjust indents, correct word, remove `$table` variable from models\Ipam.php

--- application/models/Ipam.php
+++ application/models/Ipam.php
@@ -1,26 +1,27 @@
 <?php
-class Ipam extends CI_Model {
+
+class Ipam extends CI_Model
+{
 
 
-    var $table = 'networks';
-
-
-    public function __construct() {
-                parent::__construct();
-                $this->load->database();
-        } 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
 
 
     /*======================================================
-     *Networks
+     * Networks
      *======================================================
      */
 
-    function get_all_networks() {
+    function get_all_networks()
+    {
         $this->db->order_by("networks", 'ASC');
         $query = $this->db->get("networks");
         return $query->result_array();
- 
+
         //if($query->num_rows() > 0) {
         //    return $query->result_array();
         //}
@@ -41,10 +42,9 @@ class Ipam extends CI_Model {
         $sql_limit = " limit " . $start . ", " . $limit;
         $sql = "$sql1 $sql2 $sql3 $sql_order $sql_limit";
         $query = $this->db->query($sql);
-        //return $query->result();
         return $query->result_array();
     }
-    
+
     function get_networks_count($st = NULL)
     {
         if ($st == "NIL") $st = "";
@@ -58,12 +58,13 @@ class Ipam extends CI_Model {
     }
 
 
-    function networks_search($str) {
+    function networks_search($str)
+    {
         $str = urldecode($str); // for japanese
         $this->db->like('networks', "$str");
-        $this->db->or_like('note1', $str); 
-        $this->db->or_like('note2', $str); 
-        $this->db->order_by("networks", "ASC"); 
+        $this->db->or_like('note1', $str);
+        $this->db->or_like('note2', $str);
+        $this->db->order_by("networks", "ASC");
         $query = $this->db->get('networks');
 
         if ($query->num_rows() <= 0) {
@@ -76,41 +77,41 @@ class Ipam extends CI_Model {
 
     public function networks_get_by_id($id)
     {
-        //$this->db->from($this->table);
         $this->db->from("networks");
-        $this->db->where('id',$id);
+        $this->db->where('id', $id);
         $query = $this->db->get();
- 
+
         return $query->row();
     }
- 
+
     public function networks_add($data)
     {
-        $this->db->insert($this->table, $data);
+        $this->db->insert("networks", $data);
         return $this->db->insert_id();
     }
- 
+
     public function networks_update($where, $data)
     {
-        //$this->db->update($this->table, $data, $where);
         $this->db->update("networks", $data, $where);
         return $this->db->affected_rows();
     }
- 
+
     public function networks_delete_by_id($id)
     {
         $this->db->where('id', $id);
-        $this->db->delete($this->table);
+        $this->db->delete("networks");
     }
 
-    public function networks_insert_csv($data) {
+    public function networks_insert_csv($data)
+    {
         $this->db->insert('networks', $data);
     }
 
-    function truncate_networks() {
+    function truncate_networks()
+    {
         $sql = 'delete from networks where id > 0 ';
         $this->db->query($sql);
- 
+
         $sql = 'VACUUM;';
         $this->db->query($sql);
 
@@ -121,10 +122,11 @@ class Ipam extends CI_Model {
      * Hosts
      *======================================================
     */
-    function get_all_hosts() {
+    function get_all_hosts()
+    {
         $query = $this->db->get("hosts");
         return $query->result_array();
- 
+
         //if($query->num_rows() > 0) {
         //    return $query->result_array();
         //}
@@ -144,10 +146,9 @@ class Ipam extends CI_Model {
         $sql_limit = " limit " . $start . ", " . $limit;
         $sql = "$sql1 $sql2 $sql_order $sql_limit";
         $query = $this->db->query($sql);
-        //return $query->result();
         return $query->result_array();
     }
-    
+
     function get_hosts_count($st = NULL)
     {
         if ($st == "NIL") $st = "";
@@ -160,12 +161,13 @@ class Ipam extends CI_Model {
     }
 
 
-    function hosts_search($str) {
+    function hosts_search($str)
+    {
         $str = urldecode($str); // for japanese
         $this->db->like('ip_address', "$str");
-        $this->db->or_like('host', $str); 
-        $this->db->or_like('note', $str); 
-        $this->db->order_by("hosts", "ASC"); 
+        $this->db->or_like('host', $str);
+        $this->db->or_like('note', $str);
+        $this->db->order_by("hosts", "ASC");
         $query = $this->db->get('hosts');
 
         if ($query->num_rows() <= 0) {
@@ -178,24 +180,21 @@ class Ipam extends CI_Model {
 
     public function hosts_get_by_id($id)
     {
-        //$this->db->from($this->table);
         $this->db->from("hosts");
-        $this->db->where('id',$id);
+        $this->db->where('id', $id);
         $query = $this->db->get();
- 
+
         return $query->row();
     }
 
     public function hosts_add($data)
     {
-        //$this->db->insert($this->table, $data);
         $this->db->insert("hosts", $data);
         return $this->db->insert_id();
     }
- 
+
     public function hosts_update($where, $data)
     {
-        //$this->db->update($this->table, $data, $where);
         $this->db->update("hosts", $data, $where);
         return $this->db->affected_rows();
     }
@@ -203,11 +202,11 @@ class Ipam extends CI_Model {
     public function hosts_delete_by_id($id)
     {
         $this->db->where('id', $id);
-        //$this->db->delete($this->table);
         $this->db->delete("hosts");
     }
 
-    public function hosts_insert_csv($data) {
+    public function hosts_insert_csv($data)
+    {
         $this->db->insert('hosts', $data);
     }
 
@@ -218,38 +217,34 @@ class Ipam extends CI_Model {
     */
     private function _get_datatables_query()
     {
-         
+
         $this->db->from($this->table);
- 
+
         $i = 0;
-     
+
         foreach ($this->column_search as $item) // loop column 
         {
-            if($_POST['search']['value']) // if datatable send POST for search
+            if ($_POST['search']['value']) // if datatable send POST for search
             {
-                 
-                if($i===0) // first loop
+
+                if ($i === 0) // first loop
                 {
                     $this->db->group_start(); // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
                     $this->db->like($item, $_POST['search']['value']);
-                }
-                else
-                {
+                } else {
                     $this->db->or_like($item, $_POST['search']['value']);
                 }
- 
-                if(count($this->column_search) - 1 == $i) //last loop
+
+                if (count($this->column_search) - 1 == $i) //last loop
                     $this->db->group_end(); //close bracket
             }
             $i++;
         }
-         
-        if(isset($_POST['order'])) // here order processing
+
+        if (isset($_POST['order'])) // here order processing
         {
             $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
-        } 
-        else if(isset($this->order))
-        {
+        } else if (isset($this->order)) {
             $order = $this->order;
             $this->db->order_by(key($order), $order[key($order)]);
         }
@@ -259,12 +254,11 @@ class Ipam extends CI_Model {
     function get_datatables()
     {
         $this->_get_datatables_query();
-        if($_POST['length'] != -1)
-        $this->db->limit($_POST['length'], $_POST['start']);
+        if ($_POST['length'] != -1)
+            $this->db->limit($_POST['length'], $_POST['start']);
         $query = $this->db->get("hosts");
         return $query->result();
     }
-
 
 
 }
